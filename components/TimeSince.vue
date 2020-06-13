fe81803a1219133245b9b9625ab8a5888a27c028
Erik Olsen <erawk26@gmail.com>
2020-06-13 16:38:29 -0400

    make a time-since component to show projects age

---
+++ components/TimeSince.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+    span {{elapsedTime}}
+</template>
+
+<script>
+import { formatDistance, parseISO } from 'date-fns'
+export default {
+  props: {
+    date: {
+      type: String,
+      default: new Date()
+    }
+  },
+  data() {
+    return {
+      elapsedTime: formatDistance(parseISO(this.date), new Date())
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
