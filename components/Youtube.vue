dd4cc4dcae994dea0be1727156f7033fa730a88d
Erik Olsen <erawk26@gmail.com>
2020-09-12 17:45:52 -0400

    youtube tracking and looping

---
+++ components/Youtube.vue
@@ -0,0 +1,104 @@
+<template>
+  <span class="abs-center full-height full-width">
+    <transition name="fade">
+      <v-img
+        v-if="loading || (!playerVars.loop && ended)"
+        :src="poster"
+        class="poster"
+      ></v-img>
+    </transition>
+    <youtube-media
+      ref="youtube"
+      v-if="ytId"
+      v-show="!loading"
+      @ready="onReady"
+      @playing="onPlay"
+      @error="clearProgressTracking"
+      @paused="clearProgressTracking"
+      :video-id="ytId"
+      :player-vars="playerVars"
+      :mute="playerVars.muted === 1"
+      class="full-height"
+      player-width="100%"
+      player-height="100%"
+    ></youtube-media>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    ytId: {
+      type: String,
+      default: null
+    },
+    poster: {
+      type: String,
+      default: null
+    },
+    playerVars: {
+      type: Object,
+      default: () => ({ autoplay: 1 })
+    }
+  },
+  data() {
+    return { ended: false, loading: true, interval: null, player: null }
+  },
+  methods: {
+    calculateProgress() {
+      if (this.player) {
+        const elapsed = Math.round(this.player.getCurrentTime())
+        const total = Math.round(this.player.getDuration())
+        return total - elapsed > 2 ? Math.round((elapsed / total) * 100) : 100
+      } else {
+        return 0
+      }
+    },
+    clearProgressTracking() {
+      clearInterval(this.interval)
+      this.interval = null
+    },
+    onReady(evt) {
+      this.player = evt.target
+    },
+    onEnded(evt) {
+      this.ended = true
+      if (this.playerVars.loop) {
+        this.rePlay(this.ytId)
+      } else {
+        this.clearProgressTracking()
+      }
+    },
+    onPlay() {
+      const data = this.player.getVideoData()
+      setTimeout(() => {
+        if (this.loading) {
+          this.player.seekTo(0)
+          this.loading = false
+          this.$emit('loaded', this.ytId)
+        }
+      }, 3000)
+      if (!this.interval) {
+        let lastProgress = null
+        this.interval = setInterval(() => {
+          if (
+            lastProgress !== this.calculateProgress() &&
+            this.calculateProgress()
+          ) {
+            lastProgress = this.calculateProgress()
+            console.log(
+              `YouTube Progress: Watched ${lastProgress}% of "${data.title}"`
+            )
+            if (lastProgress === 100) {
+              this.onEnded()
+            }
+          }
+        }, 3000)
+      }
+    },
+    rePlay() {
+      this.player.seekTo(0)
+    }
+  }
+}
+</script>
