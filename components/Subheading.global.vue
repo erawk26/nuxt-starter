3bdaaef46ee20c6075220aeab02e3466d2bc1b3f
Erik Olsen <erawk26@gmail.com>
2020-09-13 16:01:06 -0400

    create Subheading global component

---
+++ components/Subheading.global.vue
@@ -0,0 +1,15 @@
+<template>
+  <h5
+    v-bind="$attrs"
+    v-on="$listeners"
+    class="subheading text-uppercase font-weight-medium mt-1 mb-3"
+  >
+    <slot />
+  </h5>
+</template>
+
+<script>
+export default {
+  name: 'Subheading'
+}
+</script>
