4c22ab9b1620d25681df6b03d4564093301aa56f
Erik Olsen <erik.olsen@milespartnership.com>
2020-10-03 15:15:14 -0400

    generate deep routes

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,7 +56,7 @@ export default {
   generate: {
     async routes() {
       const { $content } = require('@nuxt/content')
-      const files = await $content()
+      const files = await $content({ deep: true })
         .only(['path'])
         .fetch()
       return files.map((file) => (file.path === '/index' ? '/' : file.path))
