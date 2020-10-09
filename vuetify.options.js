29d05e7e7835e1b3501443aabb5f148437afbf83
Erik Olsen <erik.olsen@milespartnership.com>
2020-10-09 19:08:54 -0400

    testing cache and turning off default fonts

--- vuetify.options.js
+++ vuetify.options.js
@@ -1,18 +1,22 @@
+import LRU from 'lru-cache'
+const themeCache = new LRU({
+  max: 10,
+  maxAge: 1000 * 60 * 60 // 1 hour
+})
 export default {
   icons: {
     iconfont: 'md' // default - only for display purposes
   },
-  defaultAssets: {
-    font: true,
-    icons: 'md'
-  },
+  defaultAssets: false,
+  treeShake: true,
   theme: {
     dark: false,
+    options: { themeCache },
     themes: {
       dark: {
         // primary: '#263238',
         // primary: '#342a21',
-        primary: '#3b8070'
+        primary: '#04b55c'
         // accent: '#C67171',
         // secondary: '#26A5B5',
         // success: '#4CAF50',
